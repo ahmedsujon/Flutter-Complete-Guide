ee2bb25d699a524bcb69a1b31a18998407c8cb46
ahmedsujon <sujonahmed424@gmail.com>
2022-03-05 21:41:51 +0600

    take image

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(onPressed: () {},child: Icon(Icons.add),),
-        body: Center(
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            overflow: Overflow.visible ,
-            children: <Widget>[
-              Container(
-                height: 200,
-                width: 200,
-                color: Colors.amber,
-              ),
-              Positioned(
-                bottom: -50,
-                child: CircleAvatar(
-                  radius: 50,
-                ),
-              )
-            ],
-          ),
-        ),
+        body: Center(child: Image.asset("images/wallpaper.jpg")),
       ),
     );
   }
