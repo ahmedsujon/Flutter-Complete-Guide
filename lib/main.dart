5caecdbd5f91959cfcea72071b17f232786b8f94
ahmedsujon <sujonahmed424@gmail.com>
2022-03-05 21:10:16 +0600

    List view

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Homepage(),
-    );
-  }
-}
-
-class Homepage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    // height
-    var screenheight = MediaQuery.of(context).size.height;
-    var containerheight = screenheight/2;
-    // width
-    var screenwidth = MediaQuery.of(context).size.width;
-    var containerwidth = screenwidth/2;
-
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Container(
-            height: containerheight,
-            width: containerwidth,
-            color: Colors.amber,
-        )),
+      home: Scaffold(
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text("Easy Explanation"),
+              subtitle: Text("Learn Flutter"),
+              leading: CircleAvatar(child: Icon(Icons.message)),
+              trailing: Icon(Icons.add_a_photo),
+            ),
+             ListTile(
+              title: Text("Easy Explanation"),
+              subtitle: Text("Learn Flutter"),
+              leading: CircleAvatar(child: Icon(Icons.message)),
+              trailing: Icon(Icons.add_a_photo),
+            ),
+             ListTile(
+              title: Text("Easy Explanation"),
+              subtitle: Text("Learn Flutter"),
+              leading: CircleAvatar(child: Icon(Icons.message)),
+              trailing: Icon(Icons.add_a_photo),
+            )
+          ],
+        ),
       ),
     );
   }
