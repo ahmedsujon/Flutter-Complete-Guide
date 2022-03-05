38d62be089adf1571c1fefc38f68726798f350fe
ahmedsujon <sujonahmed424@gmail.com>
2022-03-05 12:49:47 +0000

    container

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-     home: SafeArea(
-       child: Scaffold(
-         backgroundColor: Colors.deepOrange,
-       ),
-     ),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(title: Text("App Bar")),
+          body: Container(
+            height: 200,
+            width: 400,
+            decoration: BoxDecoration(color: Colors.amber),
+            child: Text(
+              "Fluter App",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.normal),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
